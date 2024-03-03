c23af52c7f67ec6ad0d95420a29f892431354acc
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-03 21:55:59 +0300

    изменил метод Index в ProductController для работы со словарём

--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineShopWebApp.Models;
-using System.Text;
 
 namespace OnlineShopWebApp.Controllers
 {
@@ -8,19 +7,12 @@ namespace OnlineShopWebApp.Controllers
     {
         public string Index(int id)
         {
-            List<Product> products = new List<Product>()
-            {
-                new Product {Id = 1, Name = "Эчпочмак", Cost = 90, Description = "Очень вкусная булочка"},
-                new Product {Id = 2, Name = "Хачапури", Cost = 125, Description = "Очень вкусная лепёшка"},
-                new Product {Id = 3, Name = "Хинкали", Cost = 300, Description = "Очень вкусные пельмени"}
-            };
-
-            Product result = products.FirstOrDefault(x => x.Id == id);
+            var result = ProductsInfo.Products[id];
 
             if (result != null)
-                return $"{result.Id}\n{result.Name}\n{result.Cost}\n{result.Description}";
-            else
-                return "Товар не найден:(";
+                return $"{id}\n{result.Name}\n{result.Cost}\n{result.Description}";
+
+            return "Товар не найден:(";
         }
     }
 }
