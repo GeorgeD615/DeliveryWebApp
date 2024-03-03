1a057e569ed7d9d1ce74ba98436e03806cefe239
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-03 21:47:45 +0300

    хранение продуктов словаре + инициализация этого словаря в стат. классе

---
+++ OnlineShop/OnlineShopWebApp/Models/ProductsInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public static class ProductsInfo
+    {
+        public static Dictionary<int, Product> Products { get; set; } = new Dictionary<int, Product>()
+        {
+            {1, new Product("Эчпочмак", 90, "Очень вкусная булочка")},
+            {2, new Product("Хачапури", 125, "Очень вкусная лепёшка")},
+            {3, new Product("Хинкали", 300, "Очень вкусные пельмени")}
+        };
+    }
+}
