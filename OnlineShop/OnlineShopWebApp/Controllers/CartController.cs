0a7c80cc6cfd499c7754a071e3dc9abd3511e2ca
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-11 18:05:57 +0300

    первая реализация корзины

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index() => View(Cart.GetAll());
+
+        public IActionResult AddProductToCart(int id)
+        {
+            Cart.AddProduct(ProductsRepository.TryGetById(id));
+            return View("Index", Cart.GetAll());
+        }
+    }
+}
---
+++ OnlineShop/OnlineShopWebApp/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public static class Cart
+    {
+        private static Dictionary<Product, int> products = new Dictionary<Product, int>();
+        public static Dictionary<Product, int> GetAll() => products;
+        public static void AddProduct(Product product) 
+        { 
+            if(products.ContainsKey(product))
+                ++products[product];
+            else
+                products[product] = 1;
+        }
+    }
+}
