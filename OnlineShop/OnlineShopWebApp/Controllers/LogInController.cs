3c86f020aeb24e6445432a8aef6b5fde9ac84f5b
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-25 18:05:01 +0300

    реализовал сбор данных при входе

---
+++ OnlineShop/OnlineShopWebApp/Controllers/LogInController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult LogIn(string login, string password, string rememberMe)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
