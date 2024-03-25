8d61a0675f1861ee7862d3e2b8f3aea61ca797f5
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-25 18:32:56 +0300

    реализовал сбор данных пользователя при регистрации

---
+++ OnlineShop/OnlineShopWebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        public IActionResult Register(string login, string password, string passwordConfirm)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
