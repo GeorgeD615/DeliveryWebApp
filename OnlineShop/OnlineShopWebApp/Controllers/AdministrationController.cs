f10c62e739f1712af1ee85460e711066413882e9
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-26 12:29:59 +0300

    реализовал меню для администратора

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AdministrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public IActionResult Orders() => View();
+        public IActionResult Users() => View();
+        public IActionResult Roles() => View();
+        public IActionResult Products() => View();
+    }
+}
