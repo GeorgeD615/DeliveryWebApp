3d905ee139dc329e65446d95a56a8395f784327b
GeorgeD615 <g.davlyatshin@gmail.com>
2024-03-30 11:21:21 +0300

    перенёс иконки в частичные представления, добавил сохранение заказов в json

--- OnlineShop/OnlineShopWebApp/Models/Orders/OrdersRepository.cs
+++ OnlineShop/OnlineShopWebApp/Models/Orders/OrdersRepository.cs
@@ -1,9 +1,29 @@
-﻿namespace OnlineShopWebApp.Models.Orders
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OnlineShopWebApp.Models.Products;
+
+namespace OnlineShopWebApp.Models.Orders
 {
     public class OrdersRepository : IOrdersRepository
     {
-        private List<Order> orders = new();
-        public void AddOrder(Order order) => orders.Add(order);
+        private static readonly string dataJsonFilePath = Directory.GetCurrentDirectory() + "\\wwwroot\\OrdersData.json";
+
+        private List<Order> orders;
+        public OrdersRepository() 
+        {
+            using var reader = new StreamReader(dataJsonFilePath);
+            orders = JsonConvert.DeserializeObject<List<Order>>(reader.ReadToEnd());
+        }
+
+        public void AddOrder(Order order) {
+            orders.Add(order);
+            SaveOrdersIntoJson();
+        }
         public Order? TryGetOrder(int orderId) => orders.FirstOrDefault(order => order.Id == orderId);
+        private void SaveOrdersIntoJson()
+        {
+            using var writer = new StreamWriter(dataJsonFilePath, false);
+            writer.Write(JsonConvert.SerializeObject(orders, Formatting.Indented));
+        }
     }
 }
