e4fa995f302df9e646fdd7a52058df0668904f49
GeorgeD615 <g.davlyatshin@gmail.com>
2024-04-03 11:20:07 +0300

    исправил валидацию адреса, исправил вывод оишбок при отправке формы на ModelOnly

--- OnlineShop/OnlineShopWebApp/Models/Users/Address.cs
+++ OnlineShop/OnlineShopWebApp/Models/Users/Address.cs
@@ -9,21 +9,24 @@ namespace OnlineShopWebApp.Models.Users
         public int UserId { get; set; }
 
         [Required(ErrorMessage = "Укажите название города")]
-        [RegularExpression(@"[а-яА-я- ]{1,30}", 
-            ErrorMessage = "Название города должно иметь длину до 30 и состоять из символов кириллицы (а-я А-Я) и символа \"-\"")]
+        [RegularExpression(@"[а-яА-я- ]{2,30}", 
+            ErrorMessage = "Название города должно иметь длину от 2 до 30 и состоять из символов кириллицы (а-я А-Я) и символа \"-\"")]
         public string City { get; set; }
 
         [Required(ErrorMessage = "Укажите название улицы/проспекта")]
-        [RegularExpression(@"[а-яА-я- ]{1,30}",
+        [RegularExpression(@"[а-яА-я- ]{2,30}",
             ErrorMessage = "Название улицы/проспекта должно иметь длину до 30 и состоять из символов кириллицы (а-я А-Я) и символа \"-\"")]
         public string Street { get; set; }
 
         [Required(ErrorMessage = "Укажите номер дома")]
+        [RegularExpression(@"[0-9а-яА-Я/]{1,7}",
+            ErrorMessage = "Номер дома должен состоять из цифр, символов кириллицы (а-я А-Я) или символа / ")]
         public string House { get; set; }
 
 
         [Required(ErrorMessage = "Укажите номер квартиры")]
-        public string Flat { get; set; } 
+        [Range(0,10000, ErrorMessage = "Номер квартиры должен находится в диапозоне от 0 до 100000")]
+        public int Flat { get; set; } 
 
         public Address()
         {
